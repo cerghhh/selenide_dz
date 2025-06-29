27356d586d515086060b61875f3eb5c125697edc
ryzov <seryoga.su@yandex.ru>
2025-06-29 12:49:41 +0300

    ignore and build.gradle

---
+++ src/test/java/gitHub/slenideRepositorySearch.java
@@ -0,0 +1,18 @@
+package gitHub;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.*;
+
+public class slenideRepositorySearch {
+
+
+    @Test
+
+}
